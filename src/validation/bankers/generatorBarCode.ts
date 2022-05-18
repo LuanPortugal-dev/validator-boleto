fac8108a25b666213e3f3f6b862a467caaad7623
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-18 02:03:43 -0300

    fixing the names

--- src/validation/bankers/generatorBarCode.ts
+++
@@ -1,8 +0,0 @@
-export function convertBarCode(digitLine: string){
-    const barcode = digitLine.replace(
-        /^(\d{4})(\d{5})\d{1}(\d{10})\d{1}(\d{10})\d{1}(\d{15})$/, 
-        '$1$5$2$3$4'
-    )
-
-    return barcode
-}
