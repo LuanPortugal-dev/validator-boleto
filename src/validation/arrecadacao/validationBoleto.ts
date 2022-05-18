5f7b95615305b907c75d59ad8965c3eb4c3c0088
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-18 01:32:42 -0300

    Validation arrecadacao boleto

---
+++ src/validation/arrecadacao/validationBoleto.ts
@@ -0,0 +1,23 @@
+import { validatorDate } from './validationDate'
+import { convertBarCode } from './generatorBarCode'
+import { validationDV } from './ValidationDV'
+
+export function validationArrecadaoBoleto(value: string) {
+    const validateDV = validationDV(value)
+
+    if(!validateDV)
+        return {
+            'StatusCode': 400,
+            'ReasonError': 'Invalid verification digits',
+        }
+
+    const resultBarCode = convertBarCode(value)
+    const resultDate = validatorDate(value)
+    //const resultAmount = validatorAmount(value)
+
+    return {
+        'barCode': resultBarCode,
+        'amount': 'resultAmount',
+        'expirationDate': resultDate
+    }
+}
