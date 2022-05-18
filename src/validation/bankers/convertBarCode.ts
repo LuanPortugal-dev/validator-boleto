463a94b94b501ba91ebff2acbaded5a902c4180b
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-18 02:00:49 -0300

    Convert barcode

---
+++ src/validation/bankers/convertBarCode.ts
@@ -0,0 +1,8 @@
+export function convertBarCode(digitLine: string){
+    const barcode = digitLine.replace(
+        /^(\d{4})(\d{5})\d{1}(\d{10})\d{1}(\d{10})\d{1}(\d{15})$/, 
+        '$1$5$2$3$4'
+    )
+
+    return barcode
+}
