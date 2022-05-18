91442a563c0d9f0acc8703341f3acc17e63c030f
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-17 23:32:58 -0300

    adding lenght error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,7 +1,17 @@
 export function errorDigitLineFormat(value: string) {
-    if ( !value.match( (/([0-9])/g) ) )
+    if ( !value.match( (/^[0-9]{47}$/) ) )
         return {
             'StatusCode': 400,
             'ReasonError':'Illegal format found in barcode'
         }
+    
+}
+
+export function errorLengthFormat(value: string) {
+    if ( value.length > 48 || value.length < 47)
+        return {
+            'StatusCode': 400,
+            'ReasonError': 'Incorrect digit line size: Must contain 47 digits or 48 digits'
+        }
+    
 }
