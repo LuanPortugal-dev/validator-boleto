eebf998a8bef47e12cf6ce8c0cefc008e46f4f6f
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-17 23:43:33 -0300

    testing the barcode converter

---
+++ __tests__/bank-boleto/convert-barCode.spec.ts
@@ -0,0 +1,19 @@
+import { generatorBarCode } from '../../src/validation/bankers/generatorBarCode'
+
+const DIGIT_LINE: string = '21290001192110001210904475617405975870000002000'
+const BAR_CODE: string = '21299758700000020000001121100012100447561740'
+
+describe('Convert to bar code', () => {
+  it('should covert a valid bar code', () => {
+    const barCode: string = generatorBarCode(DIGIT_LINE)
+
+    expect(barCode).toBe(BAR_CODE)
+  })
+
+  it('should not match a invalid bar code', () => {
+    const barCode: string = generatorBarCode(DIGIT_LINE)
+    const fakeBarCode: string = BAR_CODE + '1'
+
+    expect(barCode).not.toBe(fakeBarCode)
+  })
+})
