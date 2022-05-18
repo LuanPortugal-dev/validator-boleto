369fea7fe05ea5d1ed7ed590a99ef6eb536b8714
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-18 01:39:41 -0300

    generator Barcode arrecadacao boleto

---
+++ src/validation/arrecadacao/generatorBarCode.ts
@@ -0,0 +1,10 @@
+export function convertBarCode(digitableLine: string) {
+    let barCode = ''
+    for (let index = 0; index < 4; index++) {
+      const start = (11 * (index)) + index
+      const end = (11 * (index + 1)) + index
+      barCode += digitableLine.substring(start, end)
+    }
+    return barCode
+  }
+
