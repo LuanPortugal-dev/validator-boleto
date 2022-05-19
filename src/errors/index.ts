afe49a56545f6bad491d8f3164c4425590a2bf2f
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-19 13:42:26 -0300

    file errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,17 +1,17 @@
 export function errorDigitLineFormat(value: string) {
-    if ( !value.match( (/^[0-9]{47,48}$/) ) )
+    if (!value.match(/^[0-9]{47,48}$/))
         return {
-            'StatusCode': 400,
-            'ReasonError':'Illegal format found in barcode'
+            StatusCode: 400,
+            ReasonError: 'Illegal format found in barcode',
         }
-    
 }
 
 export function errorLengthFormat(value: string) {
-    if ( value.length > 48 || value.length < 47)
+    if (value.length > 48 || value.length < 47)
         return {
-            'StatusCode': 400,
-            'ReasonError': 'Incorrect digit line size: Must contain 47 digits or 48 digits'
+            StatusCode: 400,
+            ReasonError:
+                'Incorrect digit line size: Must contain 47 digits or 48 digits',
         }
-    
 }
+
