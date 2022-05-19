ee0e07082e98aa94d511672908574ce5005aaea0
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-19 13:36:10 -0300

    module Eleven

---
+++ src/validation/arrecadacao/moduleEleven.ts
@@ -0,0 +1,36 @@
+export function moduleEleven(
+    array: number[],
+    vd?: number,
+    fieldNumber?: number
+) {
+    let counter = 2
+
+    const reducedValue = array.reduce((prev: number, current: number) => {
+        const multipliedAmount = current * counter
+        if (counter === 9) {
+            counter = 2
+        } else {
+            counter++
+        }
+
+        return multipliedAmount + prev
+    }, 0)
+
+    const modulus = reducedValue % 11
+
+    let dac = 11 - modulus
+
+    if (dac === 0 || dac === 10 || dac === 11) {
+        dac = 1
+    }
+
+    if (!vd && dac) {
+        return dac
+    }
+
+    if (vd && dac === Number(vd)) {
+        return dac
+    } else {
+        return `Invalid verification digit of field: ${fieldNumber}`
+    }
+}
