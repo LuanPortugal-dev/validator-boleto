bffc64a6f609ccd3d8f1b87c4b2212685b82f93f
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2022-05-19 14:11:51 -0300

    removing validationDV

--- src/validation/arrecadacao/validationBoleto.ts
+++ src/validation/arrecadacao/validationBoleto.ts
@@ -1,11 +1,9 @@
 import { getConcessionariaGroups } from './generationGroups'
 import { formatAmount } from './validationAmount'
 import { convertBarCode } from './convertBarCode'
-import { validateFourFields } from './validation4Fields'
 
 export function validationArrecadaoBoleto(value: string) {
     const resultGroups= getConcessionariaGroups(value)
-    const resultValidationDv = validateFourFields(value)
 
     const resultBarCode = convertBarCode(value)
     const amount = formatAmount(resultGroups.value)
